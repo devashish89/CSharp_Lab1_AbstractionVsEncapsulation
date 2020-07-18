a0d28c5302324e54bdd95fe25741ee14a2dcfd63
Devashish Nigam <devashish.nigam89@gmail.com>
2020-07-18 20:00:17 +0530

    Code

---
+++ Lab1_AbstractionVsEncapsulation/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab1_AbstractionVsEncapsulation
+{
+    public class Customer
+    {
+        //step1: What - User should be able to add Customer (Abstraction - Show only what is necessary)
+        //step2: How - Implementation 
+        //step3: Madde those complicated methods like Validation logic etc. (Encapsulation - bring down complexity)
+        public string CustomerName { get; set; }
+        public int CustomerID { get; set; }
+
+        private bool IsValid(string value) //Encapsulation
+        {
+            return string.IsNullOrEmpty(value) ? false : true;
+        }
+        public void Insert()
+        {
+            if(IsValid(CustomerName) && IsValid(CustomerID.ToString()))
+            {
+                Console.WriteLine("Added Customer: " + CustomerName);
+            }
+            
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab1_AbstractionVsEncapsulation;
+
+namespace Main_Calling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lab1_AbstractionVsEncapsulation.Customer cust = new Lab1_AbstractionVsEncapsulation.Customer();
+            cust.CustomerID = 1;
+            cust.CustomerName = "Deva";
+
+            cust.Insert(); //Abstraction - allowing end user to see what is required for him by implementing encapsulation of complicated logic validation of Inputs etc.
+        }
+    }
+}
